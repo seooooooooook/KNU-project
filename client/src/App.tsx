1942ad6521911e1fb26ba35b432ac91907d29926
seooooooooook <80299241+seooooooooook@users.noreply.github.com>
2022-09-20 19:04:17 +0900

    MainPage.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUpPage from './pages/SingUpPage';
 import SignInPage from './pages/SignInPage';
+import MainPage from './pages/MainPage';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path='/' element={<SignUpPage />} />
+                <Route path='/' element={<MainPage />} />
                 <Route path='/signup' element={<SignUpPage />} />
                 <Route path='/signin' element={<SignInPage />} />
             </Routes>
