3a866d33c9cf60acf2eb93a12008cec56b346e48
seooooooooook <80299241+seooooooooook@users.noreply.github.com>
2022-11-24 00:36:59 +0900

    로그인 연동

--- client/src/pages/Loading.tsx
+++ client/src/pages/Loading.tsx
@@ -1,21 +1,26 @@
 import React, { useEffect } from 'react';
 import { getAccessToken } from '../connector/testConn';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import queryString from 'query-string';
 
 const Loading = () => {
     const location = useLocation();
+    const navigate =  useNavigate();
+
     const { code } = queryString.parse(location.search);
+
     const reqAccesstoken =async()=>{
         if(typeof code === 'string') {
             const res = await getAccessToken(code);
-            alert(res)
+            localStorage.setItem('access_token', JSON.stringify(res.access_token))
+            navigate('/')
         }
     }
 
     useEffect(()=>{
         if(code) reqAccesstoken();
-    },[code])
+    },[])
+
     return (
         <div>
             loading....
